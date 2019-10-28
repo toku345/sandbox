5ef029004396da0b57e8a96a563d913dcd0bff22
Fumitaka Tokumitsu <toku345@gmail.com>
2019-10-29 06:25:46 +0900

    picture texture -> video texture

--- snippets/babylonar_sample.js
+++ snippets/babylonar_sample.js
@@ -21,23 +21,28 @@ var createScene = async function() {
   const trackedNode = await tracker.addTrackableObjectAsync(0, 1, 2, 3, 5);
 
   var mat = new BABYLON.StandardMaterial("mat", scene);
-  var texture = new BABYLON.Texture("https://i.imgur.com/lXehwjZ.jpg", scene);
-  mat.diffuseTexture = texture;
+  // var texture = new BABYLON.Texture("https://i.imgur.com/lXehwjZ.jpg", scene);
+  // mat.diffuseTexture = texture;
+
+  var videoTexture = new BABYLON.VideoTexture(
+    "video",
+    ["textures/babylonjs.mp4", "textures/babylonjs.webm"],
+    scene,
+    true,
+    true
+  );
+  mat.diffuseTexture = videoTexture;
+  videoTexture.video.play();
 
   // var box = BABYLON.MeshBuilder.CreateBox("box", { size: 5.0 }, scene);
   var box = BABYLON.MeshBuilder.CreateBox(
     "box",
-    { height: 5, width: 5, depth: 0.5 },
+    { height: 5, width: 5, depth: 0.1 },
     scene
   );
   box.material = mat;
   box.parent = trackedNode;
-  box.position.z += 5.0;
-
-  // var plane = BABYLON.MeshBuilder.CreatePlane('plane', {width: 5, height: 5}, scene);
-  // plane.material = mat;
-  // plane.parent = trackedNode;
-  // plane.position.z += 5.0;
+  box.position.z += 0.1;
 
   tracker.startTracking();
 
